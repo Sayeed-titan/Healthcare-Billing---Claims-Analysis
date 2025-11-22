a9ee5f6da42d4da2ef45a05a95a4951ae1c77ea7
Sayeed-titan <kaziabu.sayeed29@gmail.com>
2025-11-22 23:54:33 +0600

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,11 @@
+-- =====================================================
+-- PROJECT 2: Healthcare Billing & Claims Analysis
+-- Domain: Healthcare Finance
+-- Objective: Analyze billing patterns, insurance claims,
+--            revenue cycle, and payment trends
+-- =====================================================
+
+CREATE DATABASE HealthcareBillingDB;
+GO
+USE HealthcareBillingDB;
+GO
